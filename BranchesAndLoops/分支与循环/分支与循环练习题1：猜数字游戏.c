d63a6daf518ddb3b79d00b5fd7d67455df784368
Davy <945825394@qq.com>
2022-07-26 22:18:42 +0800

    made a new game which is not working, remaining problems need to be soft, contained the examples of how to set up functions, how to do a comple program, how to set up a random number and limit its showing range. PS:meanwhile my VS2022 crushed a lot and it's making me upset as hell.

--- BranchesAndLoops/分支与循环/分支与循环练习题1：猜数字游戏.c
+++ BranchesAndLoops/分支与循环/分支与循环练习题1：猜数字游戏.c
@@ -1,5 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <Windows.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
 //写一个猜数字游戏
 //1.自动产生一个1-100之间的数字
@@ -8,3 +12,68 @@
 //  b.猜错了：会告诉你你猜大了还是猜小了直到猜对了。
 //3.猜对后游戏会自动重新自动产生数字让你猜
 
+
+void game()//
+{
+	//system("cls");
+	do
+	{
+		printf("Let the Game start!!!!\nType your guess here: ");
+		srand((unsigned int)time(NULL));
+		int random = rand() % 100 + 1;
+		printf("%d\n", random);
+		int input2;
+		scanf("%d", &input2);
+		do
+		{
+			if (input2 < random)
+			{
+				system("cls");
+				printf("Bigger\nYour last guess: %d\nTry again: ", input2);
+				scanf("%d", &input2);
+			}
+			else if (input2 > random)
+			{
+				system("cls");
+				printf("Smaller\nYour last guess: %d\nTry again: ", input2);
+				scanf("%d", &input2);
+			}
+		} while (input2 != random);
+		system("cls");
+		printf("Congrats! You've got it! The answer is %d\nWould you like to try again?\n(Y/N)", random);
+		char input3;
+		scanf("%s", &input3);
+		if (strcmp(input3, "Y") == 0)
+			continue;
+		else if (strcmp(input3, "Y") > 0)
+			break;
+	} while (1);
+}
+
+int main()
+{
+	printf("Sublime Game designed by Davy!\nDo you want to start your game?\n1.YES! Let's go!!!\n2.NO! I'll pass!\nType your answer here: ");
+	int input1 = 0;
+	do
+	{
+		scanf("%d", &input1);
+		if (input1 == 1)
+		{
+			system("cls");
+			game();
+			break;
+		}
+		else if (input1 == 2)
+		{
+			printf("Okay, maybe next time.");
+			break;
+		}
+		else
+		{
+			system("cls");
+			printf("%d is not an option, please try again!\n\n", input1);
+			printf("Sublime Game designed by Davy!\nDo you want to start your game?\n1.YES! Let's go!!!\n2.NO! I'll pass!\nType your answer here: ");
+		}
+	} while (input1 != 2);
+	return 0;
+}
--- BranchesAndLoops/分支与循环/循环练习5：模拟用户登录场景，且只登陆三次.c
+++ BranchesAndLoops/分支与循环/循环练习5：模拟用户登录场景，且只登陆三次.c
@@ -1,7 +1,7 @@
 //#define _CRT_SECURE_NO_WARNINGS
 //#include <stdio.h>
 //#include <string.h>
-//#include <Windows.h>
+//#include <windows.h>
 //
 ////编写代码实现，模拟用户登录情景，并且只能登录三次。（只允许输入三次密码，如果密码正确则提示登录成功，如果三次均输入错误，则退出程序
 //
@@ -13,11 +13,12 @@
 //	for(int i = 1; i < 4; i++)
 //	{
 //		printf("please enter your password: ");
-//		char typedpassword[] = { 0 };
+//		char typedpassword[20] = { 0 };//这里char typedpassword要有一个大小限制，不然会报错：run-time check failure #2 - stack around the variable “typedpassword"was corrupted.
+//		                               //暂时还不清楚为什么。
 //		scanf("%s", typedpassword);
-//		if (strcmp(typedpassword, password) == 0)
+//		if (strcmp(typedpassword, password) == 0)//该比较函数需要引入头文件string.h，一位一位的比较asc码值，只要出现某一位大于某一位，则某字符串大于某字符串
 //		{
-//			printf("Login In Success!");
+//			printf("login in success!");
 //			break;
 //		}
 //		else if (strcmp(typedpassword, password) == 1)
@@ -25,11 +26,12 @@
 //			if (i <= 2)
 //			{
 //				int n = 3 - i;
-//				printf("Incorrect Password, Please try again, you have %d chance remaining%d\n", n);
+//				printf("incorrect password, please try again, you have %d chance remaining\n", n);
 //			}
 //			else if (i == 3)
 //			{
-//				printf("You have entered you own password wrong for three times you dumb ass! Fuck off! Stop wasting my time!");
+//				system("cls");
+//				printf("you have entered you own password wrong for three times you dumb ass! fuck off! stop wasting my time!");
 //				break;
 //			}
 //		}
