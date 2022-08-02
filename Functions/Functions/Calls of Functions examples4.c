db743e377de911989afb1d0eb2baf0831fff24a2
Davy <945825394@qq.com>
2022-08-03 00:50:56 +0800

    Left a file called 'Problemo Code', the code within is identical to the one in the other file 'Calls of Functions examples2',and yet I can't get the result in 'Problemo Code';Other than those, finished some of the other examples my own, and left one last quiz of the little chapter 'calls of functions'

---
+++ Functions/Functions/Calls of Functions examples4.c
@@ -0,0 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//Code a function that can record the times of the functions being called.
--- Functions/Functions/Problemo Code.c
+++ Functions/Functions/Problemo Code.c
@@ -1,17 +1,17 @@
-//#define _CRT_SECURE_NO_WARNINGS
+//#define _crt_secure_no_warnings
 //#include <stdio.h>
-
+//
 ////code a function that can determine if a year is leap year
-
+//
+//
 //int leapyeardefine(int x)
 //{
 //	return ((x % 4 == 0 && x % 100 != 0) || (x % 400 == 0));
 //}
-//
 //int main()
 //{
 //	int year = 0;
-//	for (year = 1000; year <= 2000; year++)
+//	for (year = 1000; year <= 2000; year++);
 //	{
 //		if (leapyeardefine(year) == 1)
 //			printf("%d ", year);
