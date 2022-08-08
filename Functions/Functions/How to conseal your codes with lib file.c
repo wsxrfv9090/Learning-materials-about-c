461c99660b10ca0f7f46cf4261bbff6dc61786d2
Davy <945825394@qq.com>
2022-08-08 22:26:35 +0800

    new knowledge acquired about how to use or initiate codes for selling purposes

---
+++ Functions/Functions/How to conseal your codes with lib file.c
@@ -0,0 +1,16 @@
+//Selling your code:
+//close solutions in the menu page
+//then code
+//then run the code
+//then there is going to be a xxx.lib file in the debug file under the project file
+//U basically sell your .h and .lib file;
+//So monopoly is asured.
+//
+// 
+//If you are buying 
+//Basically quote like this:
+// 
+//#include "xxx.h"
+//#include comment (lib,"xxx.lib")
+//
+//in order to use it
