68bb6e3c52eb82d70ae21d1e9473c05453da2f3b
Davy <945825394@qq.com>
2022-08-12 20:54:19 +0800

    added some descripion for the tower of Hanoi

--- Functions/Functions/Recursion Homework1：The tower of Hanoi.c
+++ Functions/Functions/Recursion Homework1：The tower of Hanoi.c
@@ -1,1 +1,10 @@
-The tower of Hanoi:
+//The tower of Hanoi:
+//Description: The problem of tower of Hanoi is about 7 different size of circles on
+//three different pillars.
+//Let's assume three pillars are A B C, and 7 different circuls are I, II, III, IV, V
+//V, VI, VII£¬the the VII one is the biggest, I one is the smallest.
+//At first the seven circles is on the pillar A, the problem is that you have to move
+//all seven circles to the pillar C, and everytime you move, an rule is that the bigger
+//circle is always below smaller ones.
+//
+//Write a code to make this happen:
