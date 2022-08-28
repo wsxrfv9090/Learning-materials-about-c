9a6e1c9aae2b5b6f95b9a46f08cbc97e4268b584
Davy <945825394@qq.com>
2022-08-28 18:14:53 +0800

    finished function exercise1, and its so damn hard since its concered with a lot of pointers which i haven't learned just yet. Been struggling with this for a while

--- Functions/Functions/Functions exercises1.c
+++ Functions/Functions/Functions exercises1.c
@@ -1,18 +1,65 @@
-//Reorder the string in the backwards order
-//Code a function called "reverse_string(char* string)" with recursion
-//Can't use library function.
-
-//char reverse_string(char* str)
+////Reorder the string in the backwards order
+////Code a function called "reverse_string(char* string)" with recursion
+////Can't use library function.
+//
+//NORMAL WAYS OF DOING IT
+//#include <stdio.h>
+//int my_strlen(char* str)
 //{
-//	if ()
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
 //}
-#include <stdio.h>
+//void reverse_string(char* str)
+//{
+//	int left = 0;
+//	int right = my_strlen(str) - 1;
+//	while (left < right)
+//	{
+//		char tmp = str[left];
+//		str[left] = str[right];
+//		str[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char string[] = "abcdef";
+//	reverse_string(string);
+//	printf("%s", string);
+//	return 0;
+//}
+//RECURSION WAYS OF DOING IT
+#include<stdio.h>
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+void reverse_string(char* str)
+{
+	char tmp = *str;
+	int len = my_strlen(str);
+	*str = *(str + len - 1);
+	*(str + len - 1) = '\0';
+	if (my_strlen(str + 1) >= 2)
+		reverse_string(str + 1);
+	*(str + len - 1) = tmp;
+}
 int main()
 {
-	char string[20] = { 0 };
-	scanf_s("%s", string, 20);
-	int size = sizeof string / sizeof string[0];
-	printf("%d", size);
-	//printf("%s", reverse_string(string));
+	char arr[] = "abcdef";
+	reverse_string(arr);
+	printf("%s\n", arr);
 	return 0;
 }
