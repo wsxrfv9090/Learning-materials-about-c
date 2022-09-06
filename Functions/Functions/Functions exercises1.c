59012e64428876d6df6e0a77a048b63bd9469b60
Davy <945825394@qq.com>
2022-09-06 10:48:44 +0800

    some old files I've changed and last finishing on functions

--- Functions/Functions/Functions exercises1.c
+++ Functions/Functions/Functions exercises1.c
@@ -1,9 +1,43 @@
-////Reorder the string in the backwards order
-////Code a function called "reverse_string(char* string)" with recursion
-////Can't use library function.
-//
-//NORMAL WAYS OF DOING IT
-//#include <stdio.h>
+//////Reorder the string in the backwards order
+//////Code a function called "reverse_string(char* string)" with recursion
+//////Can't use library function.
+////
+////NORMAL WAYS OF DOING IT
+////#include <stdio.h>
+////int my_strlen(char* str)
+////{
+////	int count = 0;
+////	while (*str != '\0')
+////	{
+////		count++;
+////		str++;
+////	}
+////	return count;
+////}
+////void reverse_string(char* str)
+////{
+////	int left = 0;
+////	int right = my_strlen(str) - 1;
+////	while (left < right)
+////	{
+////		char tmp = str[left];
+////		str[left] = str[right];
+////		str[right] = tmp;
+////		left++;
+////		right--;
+////	}
+////}
+////int main()
+////{
+////	char string[] = "abcdef";
+////	reverse_string(string);
+////	printf("%s", string);
+////	return 0;
+////}
+////RECURSION WAYS OF DOING IT
+////Can't quite understand the logic here, I'm going to leave it until I learned
+////how to use pointers.
+//#include<stdio.h>
 //int my_strlen(char* str)
 //{
 //	int count = 0;
@@ -16,50 +50,18 @@
 //}
 //void reverse_string(char* str)
 //{
-//	int left = 0;
-//	int right = my_strlen(str) - 1;
-//	while (left < right)
-//	{
-//		char tmp = str[left];
-//		str[left] = str[right];
-//		str[right] = tmp;
-//		left++;
-//		right--;
-//	}
+//	char tmp = *str;/* is the veriable tmp = the first element of str or all of it?*/
+//	int len = my_strlen(str);
+//	*str = *(str + len - 1);//Can't understand this whole thing
+//	*(str + len - 1) = '\0';//This either
+//	if (my_strlen(str + 1) >= 2)//This either
+//		reverse_string(str + 1);//This either
+//	*(str + len - 1) = tmp;
 //}
 //int main()
 //{
-//	char string[] = "abcdef";
-//	reverse_string(string);
-//	printf("%s", string);
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
 //	return 0;
 //}
-//RECURSION WAYS OF DOING IT
-#include<stdio.h>
-int my_strlen(char* str)
-{
-	int count = 0;
-	while (*str != '\0')
-	{
-		count++;
-		str++;
-	}
-	return count;
-}
-void reverse_string(char* str)
-{
-	char tmp = *str;
-	int len = my_strlen(str);
-	*str = *(str + len - 1);
-	*(str + len - 1) = '\0';
-	if (my_strlen(str + 1) >= 2)
-		reverse_string(str + 1);
-	*(str + len - 1) = tmp;
-}
-int main()
-{
-	char arr[] = "abcdef";
-	reverse_string(arr);
-	printf("%s\n", arr);
-	return 0;
-}
--- Functions/Functions/Functions exercises2.c
+++ Functions/Functions/Functions exercises2.c
@@ -1,3 +1,15 @@
 //Culculate the sum of every number with in a big number.
-//Code a function called DigitSum(n),input a non-minus integer,and return the sum of 
+//Code a function called DigitSum(n), input a non - minus integer, and return the sum of
 //the numbers consist it.
+#include <stdio.h>
+int DigitSum(int x)
+{
+	if (DigitSum(x / 10) != 0)
+		return (x % 10 + DigitSum(x / 10));
+}
+int main()
+{
+	int num = 2459;
+	printf("%d", DigitSum(num));
+	return 0;
+}
