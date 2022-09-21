eac1a37ef21e5fc2a8a49efaf1fb33cd54581e3e
Davy <945825394@qq.com>
2022-09-21 17:30:47 +0800

    finished everything about the chapter recursion, recently been kinda overwhelmed by the schooling. But planning on catching back with in this semester.

--- Functions/Functions/Functions exercises2.c
+++ Functions/Functions/Functions exercises2.c
@@ -1,15 +1,20 @@
-//Culculate the sum of every number with in a big number.
-//Code a function called DigitSum(n), input a non - minus integer, and return the sum of
-//the numbers consist it.
-#include <stdio.h>
-int DigitSum(int x)
-{
-	if (DigitSum(x / 10) != 0)
-		return (x % 10 + DigitSum(x / 10));
-}
-int main()
-{
-	int num = 2459;
-	printf("%d", DigitSum(num));
-	return 0;
-}
+////Culculate the sum of every number with in a big number.
+////Code a function called DigitSum(n), input a non - minus integer, and return the sum of
+////the numbers consist it.
+//#include <stdio.h>
+//int DigitSum(int n)
+//{
+//	if (n > 9)
+//	{
+//		return DigitSum(n / 10) + n % 10;
+//	}
+//	else
+//		return n;
+//}
+//int main()
+//{
+//	int num = 2459;
+//	int sum = DigitSum(num);
+//	printf("%d", sum);
+//	return 0;
+//}
--- Functions/Functions/Functions exercises3.c
+++ Functions/Functions/Functions exercises3.c
@@ -1,1 +1,21 @@
-//Use recursion to culculate the k power of n
+////Use recursion to culculate the k power of n
+//
+//#include <stdio.h>
+//double Pow(int n, int k)
+//{
+//	if (k == 0)
+//		return 1;
+//	else if (k > 0)
+//		return n * Pow(n, k - 1);
+//	else
+//		return 1 / (n * Pow(n, -k - 1));
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf_s("%d %d", &n, &k, 1, 1);
+//	double ret = Pow(n, k);
+//	printf("%lf\n", ret);
+//	return 0;
+//}
