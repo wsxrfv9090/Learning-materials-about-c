bfda61c7e832bcf6bf566bee7d4ab808483e79a3
Davy <945825394@qq.com>
2022-11-09 02:45:00 +0800

    stole some codes about binary to decimal and decimal to binary transformation, its in the test.c in the exforfunctions

--- EXforFunctions/test subject.c
+++ EXforFunctions/test subject.c
@@ -1,16 +1,16 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-
-enum Clor
-{
-	Red,
-	Yellow,
-	Blue,
-	Purpel
-};
-int main()
-{
-	enum Clor ass = Red;
-	printf("ass=%s", ass);
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+//
+//enum Clor
+//{
+//	Red,
+//	Yellow,
+//	Blue,
+//	Purpel
+//};
+//int main()
+//{
+//	enum Clor ass = Red;
+//	printf("ass=%s", ass);
+//}
