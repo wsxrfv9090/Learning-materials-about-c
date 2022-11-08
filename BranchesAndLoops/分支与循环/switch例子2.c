3e118e5efe5bb37c0ad2c9d630ecf896d52ac26b
Davy <945825394@qq.com>
2022-11-08 19:10:34 +0800

    didn't actually do anything

--- BranchesAndLoops/分支与循环/switch例子2.c
+++ BranchesAndLoops/分支与循环/switch例子2.c
@@ -1,26 +1,26 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int day = 0;
-//	scanf("%d", &day);
-//	switch (day)
-//	{
-//	case 1:
-//	case 2:
-//	case 3:
-//	case 4:
-//	case 5:
-//		printf("工作日\n");
-//		break;
-//	case 6:
-//	case 7:
-//		printf("周末\n");
-//		break;
-//	default:
-//		printf("输入错误\n");
-//		break;
-//	}
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("工作日\n");
+		break;
+	case 6:
+	case 7:
+		printf("周末\n");
+		break;
+	default:
+		printf("输入错误\n");
+		break;
+	}
+	return 0;
+}
